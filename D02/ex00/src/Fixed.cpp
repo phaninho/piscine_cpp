3d7e6a61690c76a9e3454816f342ca19eed46732
phaninho <phaninho@live.fr>
2018-03-17 00:56:47 +0100

    [add] ex00 working

---
+++ D02/ex00/src/Fixed.cpp
@@ -0,0 +1,42 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+int const Fixed::_frac_bit = 8;
+
+Fixed::Fixed(void): _point_value(0)
+{
+  std::cout << "Default constructor called" << std::endl;
+  return ;
+}
+
+Fixed::Fixed(Fixed &org)
+{
+  std::cout << "Copy constructor called" << std::endl;
+  *this = org;
+  return ;
+}
+
+Fixed::~Fixed(void)
+{
+  std::cout << "Destructor called" << std::endl;
+  return ;
+}
+
+int Fixed::getRawBits( void ) const
+{
+  std::cout << "getRawBits member function called" << std::endl;
+  return (this->_point_value);
+}
+
+Fixed   &Fixed::operator=( Fixed const & rhs)
+{
+  std::cout << "Assignation operator called" << std::endl;
+  this->_point_value = rhs.getRawBits();
+  return (*this);
+}
+
+void  Fixed::setRawBits( int const raw )
+{
+  // std::cout << "Assignation operator called" << std::endl;
+  this->_point_value = raw;
+}
