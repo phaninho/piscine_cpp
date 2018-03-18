781dac9d9c703770c0f25d2ed00da367beb8aab5
phaninho <phaninho@live.fr>
2018-03-18 04:01:16 +0100

    [update] ex04 check arg values

--- D02/ex04/src/main.cpp
+++ D02/ex04/src/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <string>
+
+std::string space_remove(char *av)
+{
+  std::string str(av);
+  int len = str.length();
+  str = "";
+
+  for (int i = 0; i < len; i++)
+  {
+    if (!isspace(av[i]))
+      str += av[i];
+  }
+  return (str);
+}
+
+bool  is_operator(char c)
+{
+  if (c == '+' || c == '-' || c == '*' || c == '/' || c == '.' || c == '(' || c == ')')
+    return (1);
+  return (0);
+}
+
+bool  check_str_syntax(std::string str)
+{
+  int len = str.length();
+  for (int i = 0; i < len; i++)
+  {
+    if (!std::isdigit(str[i]))
+      if (!is_operator(str[i]))
+        return (1);
+  }
+  return (0);
+}
 
 int main(int ac, char **av)
 {
-  if (ac < 2)
-    std::cout "enter a calcul to be evaluate"
+  std::string str;
+
+  if (ac != 2)
+  {
+    std::cout << "usage: ./eval_expr \"calcul to evaluate\"" << std::endl;
+    return (1);
+  }
+  str = space_remove(av[1]);
+  if (check_str_syntax(str))
+  {
+    std::cout << "operation syntax error" << std::endl;
+    return (1);
+  }
+  std::cout << str << std::endl;
+  return (0);
 }
