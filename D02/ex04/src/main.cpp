de4c6ca43b494f46553ef3d53efcfac5f1706ee9
phaninho <phaninho@live.fr>
2018-03-18 22:53:09 +0100

    ]

--- D02/ex04/src/main.cpp
+++ D02/ex04/src/main.cpp
@@ -34,29 +34,45 @@ bool  is_operator(char c, std::string *delim_str)
   return (0);
 }
 
-bool  check_str_syntax(std::string str)
+bool  check_str_syntax(std::string *str)
 {
   int t = 0;
-  int len = str.length();
+  int len = str->length();
   std::string delim_str = "";
 
   for (int i = 0; i < len; i++)
   {
-    if (!std::isdigit(str[i]))
+    if (!std::isdigit((*str)[i]))
     {
-      if (!is_operator(str[i], &delim_str))
+      if (!is_operator((*str)[i], &delim_str))
         return (1);
-      t = 1;
+        t = 1;
     }
-    delim_str += str[i];
+    delim_str += (*str)[i];
     if (t == 1)
     {
       t = 0;
       delim_str += '_';
     }
   }
-  std::cout << delim_str << std::endl;
+  *str = delim_str;
   return (0);
+}
+
+int sym_count(std::string str)
+{
+  int count = 0;
+
+  for (size_t i = 0; i < str.length(); i++)
+    if (str[i] == '_')
+      count++;
+  return (count);
+}
+
+void make_operation(std::string *array, int sym_nb)
+{
+  for (int i = 0; i <= sym_nb; i++)
+    std::cout << array[i] << std::endl;
 }
 
 int main(int ac, char **av)
@@ -69,11 +85,17 @@ int main(int ac, char **av)
     return (1);
   }
   str = space_remove(av[1]);
-  if (check_str_syntax(str))
+  if (check_str_syntax(&str))
   {
     std::cout << "operation syntax error" << std::endl;
     return (1);
   }
-  std::cout << str << std::endl;
+  int sym_nb = sym_count(str);
+  std::istringstream ss(str);
+  std::string array[sym_nb];
+  int i = 0;
+  while(std::getline(ss, array[i], '_'))
+    i++;
+  make_operation(array, sym_nb);
   return (0);
 }
