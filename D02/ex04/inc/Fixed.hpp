f3eea49486777ed490cb605ac228d1f707428989
phaninho <phaninho@live.fr>
2018-03-18 03:15:15 +0100

    [add]ex04 first elements

---
+++ D02/ex04/inc/Fixed.hpp
@@ -0,0 +1,51 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+  public:
+    Fixed(void);
+    Fixed(int const nb);
+    Fixed(float const nb);
+    Fixed(Fixed const &f);
+    ~Fixed(void);
+
+    void  setRawBits( int const raw );
+    int   getRawBits( void ) const;
+    int   toInt( void ) const;
+    float toFloat( void ) const;
+
+    bool operator>( Fixed const & rhs);
+    bool operator<( Fixed const & rhs);
+    bool operator>=( Fixed const & rhs);
+    bool operator<=( Fixed const & rhs);
+    bool operator==( Fixed const & rhs);
+    bool operator!=( Fixed const & rhs);
+
+    Fixed operator+( Fixed const & rhs);
+    Fixed operator-( Fixed const & rhs);
+    Fixed operator*( Fixed const & rhs);
+    Fixed operator/( Fixed const & rhs);
+    Fixed &operator=( Fixed const & rhs);
+
+    Fixed &operator++(void);
+    Fixed operator++(int);
+    Fixed &operator--(void);
+    Fixed operator--(int);
+
+    static Fixed min(Fixed & lhs, Fixed & rhs);
+    static Fixed max(Fixed & lhs, Fixed & rhs);
+    static Fixed const min(Fixed const & lhs, Fixed const & rhs);
+    static Fixed const max(Fixed const & lhs, Fixed const & rhs);
+
+  private:
+    int _point_value;
+    static int const _frac_bit;
+};
+
+std::ostream  &operator<<(std::ostream & o, Fixed const & rhs);
+
+
+#endif
---
+++ D02/ex04/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+  if (ac < 2)
+    std::cout "enter a calcul to be evaluate"
+}
