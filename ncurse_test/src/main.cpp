2c9f3d45ef5b754a0055d44c7ed0902198658600
phaninho <phaninho@live.fr>
2018-03-30 18:06:43 +0200

    *

--- ncurse_test/src/main.cpp
+++ ncurse_test/src/main.cpp
@@ -17,18 +17,21 @@ int main()
     int ch = 0;
     int posx;
     int posy;
-    int xmax, ymax;
+    int xmax, ymax, xcmp, ycmp;
 
     initscr();
     
     refresh();
     getmaxyx(stdscr, ymax, xmax);
+    xcmp = xmax;
+    ycmp = ymax;
 
     Box b(ymax - (ymax / 3) , xmax - (xmax / 3), 3, 5);    
     WINDOW *win =newwin(b.height, b.width, b.start_y, b.start_x);
     keypad(stdscr, TRUE);
    
     display_win(win);
+    
     posx = 7;
     posy = (ymax / 3);
     move((ymax - (ymax / 3)) / 2 , (xmax / 4));    
@@ -36,7 +39,14 @@ int main()
     
     while (ch != 27)
     {
-    
+        refresh();
+        getmaxyx(stdscr, ycmp, xcmp);
+        if (ycmp != ymax || xcmp != xmax)
+        {
+            move((ycmp - (ycmp / 3)) / 2 , (xcmp / 4));    
+            printw("win change size");
+
+        }
         ch = getch();
         noecho();
         // std::cout << "getch = " <<  ch;
@@ -47,6 +57,8 @@ int main()
             move(posy, posx);
             while (ch != 27)
             {
+                getmaxyx(stdscr, ymax, xmax);
+                // display_spacecarft();
                 ch = getch();
                 if(ch == KEY_DOWN && posy < b.height + (b.start_y - 2))
                     posy++;
@@ -55,7 +67,13 @@ int main()
                 else if(ch == KEY_RIGHT && posx < b.start_x + (b.width - 2))
                     posx++;
                 else if(ch == KEY_LEFT && posx > b.start_x + 1)
-                    posx--;    
+                    posx--; 
+                if (COLS < 50 || LINES < 50)
+                {
+                    endwin();
+                    printf("win change size");
+                    
+                }   
                 noecho();
                 move(posy, posx);            
             }
